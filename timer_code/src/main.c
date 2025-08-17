545622ff3380a7ddcbc857b628195f531f03b399
Juanma <juanmaschilling@gmail.com>
2025-08-17 10:29:26 -0400

    hello world compiles and works

--- timer_code/src/main.c
+++ timer_code/src/main.c
@@ -1,1 +1,26 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/usb/usb_device.h>
 
+const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+int main(void)
+{
+    if (!device_is_ready(dev)) {
+        printk("Device not ready\n");
+        return -1;
+    }
+
+    // INFO: only necessary with adafruit
+    if (usb_enable(NULL)) {
+        printk("USB enable failed\n");
+        return -1;
+    }
+
+    printk("This is the encoder sample code!\n");
+    while (1) {
+        k_sleep(K_SECONDS(3));
+        printk("This is the encoder sample code!\n");
+    }
+}
---
+++ todo.md
@@ -0,0 +1,8 @@
+# Things to do on the project
+- [ ] Make a working clock to keep track of time
+- [ ] Add apropriate clang format to the project
+- [ ] Add gitignore
+- [ ] Fix virtual environment issue
+- [ ] Deps should be inside of timer alarm not  inside git
+
+
